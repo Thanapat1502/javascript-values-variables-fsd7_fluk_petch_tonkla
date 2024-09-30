9b7cbae33df111319f9327545a8b8424f0c1f379
Arkorn Pattanavibul <arkorn.pat@gmail.com>
2024-09-30 13:24:11 +0700

    fix ex-1

--- ex-1and2.js
+++ ex-1and2.js
@@ -3,9 +3,9 @@
 let firstName = "Karter"
 let lastName = "Anderson"
 let adddress = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
-let restaurantName = "Chef's Table by Chef James"
-let netFoodBillAmount = 15000
-let openRestaurantsCount = 1500
+const restaurantName = "Chef's Table by Chef James"
+const netFoodBillAmount = 15000
+const openRestaurantsCount = 1500
 //Exercise #2: Variable Reassignment
 //Start coding here
 adddress = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
